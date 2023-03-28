621b930306b9ba3714a6a8dcdcfc036408e31d39
iamjabeed <smartyjabeedbhai001@gmail.com>
2023-03-28 11:12:57 +0530

    constants page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Stack } from '@mui/system'
+import { Link } from 'react-router-dom'
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <Stack>
+
+    </Stack>
   )
 }
 
